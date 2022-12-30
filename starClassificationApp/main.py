7b03ccafae669af640afc43da5244f9469e2b37f
Gabriele Marino <45176656+gabrielemarino-gm@users.noreply.github.com>
2022-12-30 12:08:26 +0100

    fix path error

--- starClassificationApp/main.py
+++ starClassificationApp/main.py
@@ -3,10 +3,17 @@ import tkinter as tk
 import pickle
 import pandas as pd
 
+# file = 'Users/gabriele/Desktop/Università/Primo Anno/Data Mining and Machine Learnig/StellarClassificator/starClassificationApp/model.pkl'
+file = 'model.pkl'
+
 # Crea la finestra principale
 root = tk.Tk()
 root.geometry("350x500+50+50")
 
+# 'dec', 'u', 'g', 'r', 'i', 'z', 'specobjid', 'redshift', 'plate', 'mjd'
+
+label_dec = tk.Label(root, text = "dec:")
+label_u = tk.Label(root, text = "u:")
 label_g = tk.Label(root, text = "g:")
 label_r = tk.Label(root, text = "r:")
 label_i = tk.Label(root, text = "i:")
@@ -16,6 +23,8 @@ label_redshift = tk.Label(root, text = "redshift:")
 label_plate = tk.Label(root, text = "plate:")
 label_mjd = tk.Label(root, text = "mjd:")
 
+entry_dec = tk.Entry(root)
+entry_u = tk.Entry(root)
 entry_g = tk.Entry(root)
 entry_r = tk.Entry(root)
 entry_i = tk.Entry(root)
@@ -26,39 +35,47 @@ entry_plate = tk.Entry(root)
 entry_mjd = tk.Entry(root)
 
 # Posiziona la etichetta e il pulsante nella finestra
-label_g.grid(row=0, column=0, pady=10, padx=10, sticky="e")
-entry_g.grid(row=0, column=1)
+label_dec.grid(row=0, column=0, pady=10, padx=10, sticky="e")
+entry_dec.grid(row=0, column=1)
 
-label_r.grid(row=1, column=0, pady=10, padx=10, sticky="e")
-entry_r.grid(row=1, column=1)
+label_u.grid(row=1, column=0, pady=10, padx=10, sticky="e")
+entry_u.grid(row=1, column=1)
 
-label_i.grid(row=2, column=0, pady=10, padx=10, sticky="e")
-entry_i.grid(row=2, column=1)
+label_g.grid(row=2, column=0, pady=10, padx=10, sticky="e")
+entry_g.grid(row=2, column=1)
 
-label_z.grid(row=3, column=0, pady=10, padx=10, sticky="e")
-entry_z.grid(row=3, column=1)
+label_r.grid(row=3, column=0, pady=10, padx=10, sticky="e")
+entry_r.grid(row=3, column=1)
 
-label_specobjid.grid(row=4, column=0, pady=10, padx=10)
-entry_specobjid.grid(row=4, column=1)
+label_i.grid(row=4, column=0, pady=10, padx=10, sticky="e")
+entry_i.grid(row=4, column=1)
 
-label_redshift.grid(row=5, column=0, pady=10, padx=10)
-entry_redshift.grid(row=5, column=1)
+label_z.grid(row=5, column=0, pady=10, padx=10, sticky="e")
+entry_z.grid(row=5, column=1)
 
-label_plate.grid(row=6, column=0, pady=10, padx=10)
-entry_plate.grid(row=6, column=1)
+label_specobjid.grid(row=6, column=0, pady=10, padx=10)
+entry_specobjid.grid(row=6, column=1)
 
-label_mjd.grid(row=7, column=0, pady=10, padx=10)
-entry_mjd.grid(row=7, column=1)
+label_redshift.grid(row=7, column=0, pady=10, padx=10)
+entry_redshift.grid(row=7, column=1)
+
+label_plate.grid(row=8, column=0, pady=10, padx=10)
+entry_plate.grid(row=8, column=1)
+
+label_mjd.grid(row=9, column=0, pady=10, padx=10)
+entry_mjd.grid(row=9, column=1)
 
 label_res = tk.Label(root)
-label_res.grid(row=9, column=1, pady=10, padx=10)
+label_res.grid(row=11, column=1, pady=10, padx=10)
 
 def start():
     # Apri il file in modalità di lettura binaria
-    with open('model.pkl', 'rb') as f:
+    with open(file, 'rb') as f:
         # Carica il modello dal file
         classifier = pickle.load(f)
 
+    dec = float(entry_dec.get())
+    u = float(entry_u.get())
     g = float(entry_g.get())
     r = float(entry_r.get())
     i = float(entry_i.get())
@@ -70,6 +87,8 @@ def start():
 
     df = pd.DataFrame(
     {
+        "dec": [dec],
+        "u": [u],
         "g": [g],
         "r": [r],
         "i": [i],
@@ -105,8 +124,8 @@ def reset():
 buttonReset = tk.Button(root, text = "Reset", command=reset)
 buttonStart = tk.Button(root, text = "Start", command=start)
 
-buttonReset.grid(row=8, column=1, pady=10, padx=10, sticky="sw")
-buttonStart.grid(row=8, column=1, pady=10, padx=10, sticky="se")
+buttonReset.grid(row=10, column=1, pady=10, padx=10, sticky="sw")
+buttonStart.grid(row=10, column=1, pady=10, padx=10, sticky="se")
 
 
 # Avvia il ciclo di eventi di Tkinter
